50e7de6d8d4e272b9f7de833397cca642e1dbef6
David Vreony <dpvreony@users.noreply.github.com>
2019-12-27 14:27:41 +0000

    echo now works

--- src/CleoSystem.EchoWebService.WebApp/Startup.cs
+++ src/CleoSystem.EchoWebService.WebApp/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Primitives;
 
 namespace CleoSystem.EchoWebService.WebApp
 {
@@ -69,18 +70,31 @@ namespace CleoSystem.EchoWebService.WebApp
         {
             var requestBodyStream = new MemoryStream();
 
+            var response = context.Response;
+            response.StatusCode = 501;
+            response.ContentType = "text/plain";
+
+            var request = context.Request;
+            await response.WriteAsync($"{request.Method} : {request.Path}{request.QueryString.Value}\n")
+                .ConfigureAwait(false);
+
+            foreach (KeyValuePair<string, StringValues> requestHeader in context.Request.Headers)
+            {
+                var values = string.Join(";", requestHeader.Value);
+
+                await response.WriteAsync($"{requestHeader.Key} : {values}\n")
+                    .ConfigureAwait(false);
+            }
+
             await context.Request.Body.CopyToAsync(requestBodyStream);
             requestBodyStream.Seek(0, SeekOrigin.Begin);
 
             var requestBodyText = new StreamReader(requestBodyStream).ReadToEnd();
 
-            var response = context.Response;
 
             await response.WriteAsync(requestBodyText)
                 .ConfigureAwait(false);
 
-            response.StatusCode = 501;
-            response.ContentType = "text/plain";
         }
 
         private static bool Predicate(HttpContext arg)
